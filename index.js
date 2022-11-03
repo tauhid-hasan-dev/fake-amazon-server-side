869e5b57dd065ce5c22cde2a68aa7785b758d17b
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-03 17:07:46 +0300

    project setup completed, connected with mongodb, all products api created

---
+++ index.js
@@ -0,0 +1,42 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running for Fake Amazon Website')
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jjvuikj.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    const productsCollection = client.db('fakeAmazon').collection('products');
+
+    try {
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+
+
+}
+run().catch(err => console.log(err))
+
+app.listen(port, () => {
+    console.log(`Fake amazon server is running on ${port}`)
+})
